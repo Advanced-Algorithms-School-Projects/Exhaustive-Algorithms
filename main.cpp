372daf86ceeb641ab9ac55723d6455923a0d1642
Patrick Joseph Hanbury <hanbury.p@northeastern.edu>
2018-05-12 14:34:39 -0400

    created main.cpp

---
+++ main.cpp
@@ -0,0 +1,18 @@
+//Patrick Hanbury and Cassie Smith
+//Advanced Algo Project 1 - Exhuastive Algorithms
+//
+//May 12, 2018
+//main.cpp
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    
+    cout << "hello " << endl;
+
+
+ return 0;   
+}
