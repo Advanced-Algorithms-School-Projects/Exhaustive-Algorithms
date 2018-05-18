da43de609516e59bac1a51204cb2caddbce354a6
cassiels636 <35430596+cassiels636@users.noreply.github.com>
2018-05-17 23:40:58 -0400

    Graph coloring works...maybe

--- Project1a/p1b.cpp
+++ Project1a/p1b.cpp
@@ -20,6 +20,9 @@ struct EdgeProperties;
 typedef adjacency_list<vecS, vecS, bidirectionalS, VertexProperties, EdgeProperties> Graph;
 
 int exhaustiveColoring(Graph &g, int numColors, int t);
+void RGT(Graph &g, Graph &bestG, int &bestGConflicts, int numColors, Graph::vertex_iterator start, pair<Graph::vertex_iterator, Graph::vertex_iterator> range);
+void checkConflicts(Graph &g, Graph &bestG, int &bestGConflicts);
+void printSolution(Graph &g, int numConflicts, string filePath_output);
 
 struct VertexProperties
 {
@@ -107,7 +110,10 @@ int main()
 		// cout << g;
 
 		numConflicts = exhaustiveColoring(g, numColors, 600);
-		//printSolution(g, numConflicts);
+		string output = "/Users/Cassie/source/color12-3.output";
+		printSolution(g, numConflicts, output);
+		system("pause");
+
 
 	}
 	catch (indexRangeError &ex)
@@ -121,5 +127,90 @@ int main()
 }
 
 int exhaustiveColoring(Graph &g, int numColors, int t) {
+	//gets start and end vertex iterators (which allow you to access the vertex)
+	pair<Graph::vertex_iterator, Graph::vertex_iterator> vItrRange = vertices(g);
 
+	Graph::vertex_iterator firstNode = vItrRange.first;
+	Graph::vertex_iterator lastNode = vItrRange.second;
+
+	//assign all vertex colors to 0, meaning lack of color
+	for (Graph::vertex_iterator vItr = firstNode; vItr != lastNode; ++vItr) {
+		g[*vItr].color = 0;
+	}
+
+	int i = 0;
+	for (Graph::vertex_iterator vItr = firstNode; vItr != lastNode; ++vItr) {
+		cout << "Vertex " << i << " color - " << g[*vItr].color << endl;
+		i++;
+	}
+	system("pause");
+
+	Graph bestG = g;
+	int bestGConflicts = 0;
+	
+	RGT(g, bestG, bestGConflicts, numColors, firstNode, vItrRange);
+	return bestGConflicts;
+}
+
+void RGT(Graph &g, Graph &bestG, int &bestGConflicts, int numColors, Graph::vertex_iterator start, pair<Graph::vertex_iterator, Graph::vertex_iterator> range) {
+	for (int x = 1; x <= numColors; x++) {
+		g[*start].color = x; //set color of graph for start node to x
+		
+		/*pair<Graph::vertex_iterator, Graph::vertex_iterator> vItrRange = vertices(g);
+		Graph::vertex_iterator firstNode = vItrRange.first;
+		Graph::vertex_iterator lastNode = vItrRange.second;
+		int i = 0;
+		for (Graph::vertex_iterator vItr = firstNode; vItr != lastNode; ++vItr) {
+			cout << "Vertex " << i << " color - " << g[*vItr].color << endl;
+			i++;
+		}
+		system("pause");*/
+
+		//cout << " 1 " << endl;
+							 //check if start is the last night
+		if ((start + 1) >= range.second) {  //this might not work....let me know if so
+		//	cout << " 2 " << endl;
+			checkConflicts(g, bestG, bestGConflicts);
+		}
+
+		//otherwise call function recursively
+		else {
+		//	cout << " 3 " << endl;
+			RGT(g, bestG, bestGConflicts, numColors, ++start, range);
+		}
+		//cout << " 4 " << endl;
+		g[*start].color = 0;
+	}
+	return;
+}
+
+void checkConflicts(Graph &g, Graph &bestG, int &bestGConflicts) {
+	int conflicts = 0, color1 = 0, color2 = 0;
+	pair<Graph::edge_iterator, Graph::edge_iterator> eItrRange = edges(g);
+
+	for (Graph::edge_iterator eItr = eItrRange.first; eItr != eItrRange.second; ++eItr) {
+		color1 = g[target(*eItr, g)].color;
+		color2 = g[source(*eItr, g)].color;
+
+		if (color1 == color2) {
+			conflicts++;
+		}
+	}
+
+	if (conflicts < bestGConflicts) {
+		bestG = g;
+	}
+}
+
+void printSolution(Graph &g, int numConflicts, string filePath_output) {
+	ofstream myfile;
+	myfile.open(filePath_output.c_str());
+
+	myfile << "Total Conflicts: " << numConflicts << endl;
+
+	for (int counter = 0; counter < num_vertices(g); counter++)	{
+		myfile << counter << ": " << g[counter].color << endl;
+	}
+
+	myfile.close();
 }
