854ccafeb4f4eb6e0ec86338f2d381996423a216
cassiels636 <35430596+cassiels636@users.noreply.github.com>
2018-05-18 11:54:34 -0400

    Graph coloring finished

--- Project1a/p1b.cpp
+++ Project1a/p1b.cpp
@@ -4,6 +4,7 @@
 #include <limits.h>
 #include "d_except.h"
 #include <fstream>
+#include <time.h>
 
 #include <boost/graph/adjacency_list.hpp>
 
@@ -20,7 +21,7 @@ struct EdgeProperties;
 typedef adjacency_list<vecS, vecS, bidirectionalS, VertexProperties, EdgeProperties> Graph;
 
 int exhaustiveColoring(Graph &g, int numColors, int t);
-void RGT(Graph &g, Graph &bestG, int &bestGConflicts, int numColors, Graph::vertex_iterator start, pair<Graph::vertex_iterator, Graph::vertex_iterator> range);
+void RGT(Graph &g, Graph &bestG, int &bestGConflicts, int numColors, Graph::vertex_iterator start, clock_t startT, int t);
 void checkConflicts(Graph &g, Graph &bestG, int &bestGConflicts);
 void printSolution(Graph &g, int numConflicts, string filePath_output);
 
@@ -82,7 +83,7 @@ int main()
 	// Read the name of the graph from the keyboard or
 	// hard code it here for testing.
 
-	fileName = "color12-3.input";
+	fileName = "color192-7.input";
 
 	//   cout << "Enter filename" << endl;
 	//   cin >> fileName;
@@ -107,13 +108,10 @@ int main()
 		cout << "Num edges: " << num_edges(g) << endl;
 		cout << endl;
 
-		// cout << g;
-
 		numConflicts = exhaustiveColoring(g, numColors, 600);
-		string output = "/Users/Cassie/source/color12-3.output";
+		string output = "/Users/Cassie/source/color192-7.output";
 		printSolution(g, numConflicts, output);
 		system("pause");
-
 
 	}
 	catch (indexRangeError &ex)
@@ -126,7 +124,11 @@ int main()
 	}
 }
 
+//Exhaustive calculates the least number of conflicts in a graph coloring given the
+//number of colors and a graph. Exits function if the time taken exceeds t.
 int exhaustiveColoring(Graph &g, int numColors, int t) {
+	clock_t startTime = clock();
+
 	//gets start and end vertex iterators (which allow you to access the vertex)
 	pair<Graph::vertex_iterator, Graph::vertex_iterator> vItrRange = vertices(g);
 
@@ -138,56 +140,56 @@ int exhaustiveColoring(Graph &g, int numColors, int t) {
 		g[*vItr].color = 0;
 	}
 
-	int i = 0;
-	for (Graph::vertex_iterator vItr = firstNode; vItr != lastNode; ++vItr) {
-		cout << "Vertex " << i << " color - " << g[*vItr].color << endl;
-		i++;
-	}
-	system("pause");
-
 	Graph bestG = g;
-	int bestGConflicts = 0;
+	int bestGConflicts = num_edges(g);
 	
-	RGT(g, bestG, bestGConflicts, numColors, firstNode, vItrRange);
+	//Calculates least number of conflicts recursively
+	RGT(g, bestG, bestGConflicts, numColors, firstNode, startTime, t);
+	
+	//Returns least number of conflicts and the graph coloring that corresponds to it
+	g = Graph(bestG);
 	return bestGConflicts;
 }
 
-void RGT(Graph &g, Graph &bestG, int &bestGConflicts, int numColors, Graph::vertex_iterator start, pair<Graph::vertex_iterator, Graph::vertex_iterator> range) {
+//Recursively tests every coloring combination of a graph and tracks the combination with the least number of conflicts.
+//Exits the function if the time taken exceeds t.
+void RGT(Graph &g, Graph &bestG, int &bestGConflicts, int numColors, Graph::vertex_iterator start, clock_t startT, int t) {
+	pair<Graph::vertex_iterator, Graph::vertex_iterator> range = vertices(g);
+	
+	//Exit function if time is exceeded
+	if (((float)(clock() - startT) / CLOCKS_PER_SEC) >= t) {
+		return;
+	}
 	for (int x = 1; x <= numColors; x++) {
 		g[*start].color = x; //set color of graph for start node to x
-		
-		/*pair<Graph::vertex_iterator, Graph::vertex_iterator> vItrRange = vertices(g);
-		Graph::vertex_iterator firstNode = vItrRange.first;
-		Graph::vertex_iterator lastNode = vItrRange.second;
-		int i = 0;
-		for (Graph::vertex_iterator vItr = firstNode; vItr != lastNode; ++vItr) {
-			cout << "Vertex " << i << " color - " << g[*vItr].color << endl;
-			i++;
-		}
-		system("pause");*/
 
-		//cout << " 1 " << endl;
-							 //check if start is the last night
-		if ((start + 1) >= range.second) {  //this might not work....let me know if so
-		//	cout << " 2 " << endl;
+        //check if start is the last node
+		if ((start + 1) >= range.second) {
 			checkConflicts(g, bestG, bestGConflicts);
 		}
-
 		//otherwise call function recursively
 		else {
-		//	cout << " 3 " << endl;
-			RGT(g, bestG, bestGConflicts, numColors, ++start, range);
+			RGT(g, bestG, bestGConflicts, numColors, start + 1, startT, t);
 		}
-		//cout << " 4 " << endl;
-		g[*start].color = 0;
+
+		//Exit function if time is exceeded
+		if (((float)(clock() - startT) / CLOCKS_PER_SEC) >= t) {
+			return;
+		}		
 	}
+
+	//Reset node to 0 meaning lack of color
+	g[*start].color = 0;
 	return;
 }
 
+//Compares a graph coloring to the current best graph coloring and replaces it
+//if it has less conflicts.
 void checkConflicts(Graph &g, Graph &bestG, int &bestGConflicts) {
-	int conflicts = 0, color1 = 0, color2 = 0;
+	int conflicts = 0, color1, color2;
 	pair<Graph::edge_iterator, Graph::edge_iterator> eItrRange = edges(g);
 
+	//Adds up all conflicts
 	for (Graph::edge_iterator eItr = eItrRange.first; eItr != eItrRange.second; ++eItr) {
 		color1 = g[target(*eItr, g)].color;
 		color2 = g[source(*eItr, g)].color;
@@ -197,11 +199,14 @@ void checkConflicts(Graph &g, Graph &bestG, int &bestGConflicts) {
 		}
 	}
 
+	//Compares conflicts to current least number of conflicts
 	if (conflicts < bestGConflicts) {
-		bestG = g;
+		bestGConflicts = conflicts;
+		bestG = Graph(g);
 	}
 }
 
+//Prints the best graph coloring calculated
 void printSolution(Graph &g, int numConflicts, string filePath_output) {
 	ofstream myfile;
 	myfile.open(filePath_output.c_str());
